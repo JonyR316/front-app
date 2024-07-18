fb034787471d3486aa656be91532ccb554125728
JonyR316 <jlc.rodriguez316@gmail.com>
2024-07-18 05:37:52 -0500

    termine portada

---
+++ models/Product.js
@@ -0,0 +1,12 @@
+import mongoose, { model, models, Schema } from "mongoose";
+
+const ProductSchema = new Schema({
+  title: { type: String, required: true },
+  descripcion: String,
+  precio: { type: Number, required: true },
+  images: [{ type: String }],
+  category: { type: mongoose.Types.ObjectId, ref: "Category" },
+  properties: { type: Object },
+});
+
+export const Product = models.Product || model("Product", ProductSchema);
