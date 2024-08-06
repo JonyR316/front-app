b1dfd4bcc206b75d9039c6210fc1f9e6be2998d1
JonyR316 <jlc.rodriguez316@gmail.com>
2024-08-06 06:25:05 -0500

    ultimo commit

--- components/FooterU.js
+++ components/FooterU.js
@@ -3,25 +3,28 @@ import {
   FaFacebook,
   FaInstagram,
   FaWhatsapp,
+  FaPhoneAlt,
+  FaMobileAlt,
+  FaEnvelope,
   FaMapMarkerAlt,
 } from "react-icons/fa";
 import styled from "styled-components";
+import Center from "./Center";
 
 const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
-  padding: 20px;
-  margin-top: px;
+  margin-top: 10px;
 `;
 
 const FooterContainer = styled.footer`
-  background-color: #222;
+  background-color: #111;
   color: white;
-  padding: 20px 0;
+  padding: 10px 0;
   text-align: center;
   border-top: 1px solid black; /* Línea delgada en la parte superior */
-  margin-top: 30px; /* Permite que el footer se mantenga en la parte inferior */
+  margin-top: 30px;
 `;
 
 const SocialIcons = styled.div`
@@ -29,11 +32,11 @@ const SocialIcons = styled.div`
   justify-content: center;
   gap: 15px;
   margin-bottom: 20px;
-  margin-top: 10px;
+  margin-top: 20px;
 
   a {
     color: white;
-    font-size: 24px;
+    font-size: 30px;
     transition: color 0.3s;
 
     &:hover {
@@ -59,13 +62,13 @@ const InfoItem = styled.div`
   width: 150px; /* Ancho fijo para alineación */
 `;
 
-const InfoText = styled.div`
-  margin-top: 10px; /* Espacio entre el título y el párrafo */
-`;
+const InfoText = styled.div``;
+
 const LeftContent = styled.div`
   flex: 1;
   min-width: 300px;
   margin-right: 20px; // Espacio de 20px entre la parte izquierda y derecha
+  text-align: left; // Alinear texto a la izquierda
 `;
 
 const RightContent = styled.div`
@@ -74,42 +77,96 @@ const RightContent = styled.div`
   margin-left: 20px; // Espacio de 20px entre la parte derecha y el borde del contenedor
 `;
 
+const ContactItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  margin-bottom: 20px;
+  margin-top: 20px;
+`;
+const Titulof = styled.div`
+  font-size: 20px;
+`;
+const Ultimo = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+  font-size: 15px;
+`;
+
 const FooterU = () => {
   return (
     <FooterContainer>
-      <Container>
-        <LeftContent></LeftContent>
-        <RightContent>
-          <InfoText>
-            <span>
-              <b>ENCUENTRANOS TAMBIEN EN</b>
-            </span>
-          </InfoText>
-          <SocialIcons>
-            <a
-              href="https://www.facebook.com/profile.php?id=100066712185951"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaFacebook />
-            </a>
-            <a
-              href="https://www.instagram.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaInstagram />
-            </a>
-            <a
-              href="https://www.whatsapp.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaWhatsapp />
-            </a>
-          </SocialIcons>
-        </RightContent>
-      </Container>
+      <Center>
+        <Container>
+          <LeftContent>
+            <InfoText>
+              <Titulof>
+                <b>INFORMACION DE CONTACTO</b>
+              </Titulof>
+            </InfoText>
+            <ContactItem>
+              <FaPhoneAlt size={24} />
+              <span>
+                <b>Teléfono:</b> 02 5166 152
+              </span>
+            </ContactItem>
+            <ContactItem>
+              <FaMobileAlt size={26} />
+              <span>
+                <b>Celular:</b> 096 300 1034
+              </span>
+            </ContactItem>
+            <ContactItem>
+              <FaEnvelope size={40} />
+              <span>
+                <b>Correo electrónico:</b> info@maquinasecuador.com /
+                maquinas.ecuador1@gmail.com
+              </span>
+            </ContactItem>
+            <ContactItem>
+              <FaMapMarkerAlt size={24} />
+              <span>
+                <b>Dirección:</b> Sector Edén del Valle. Calle A Quito y Simón
+                Bolívar
+              </span>
+            </ContactItem>
+          </LeftContent>
+          <RightContent>
+            <InfoText>
+              <Titulof>
+                <b>ENCUÉNTRANOS TAMBIÉN EN</b>
+              </Titulof>
+            </InfoText>
+            <SocialIcons>
+              <a
+                href="https://www.facebook.com/profile.php?id=100066712185951"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaFacebook />
+              </a>
+              <a
+                href="https://www.instagram.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaInstagram />
+              </a>
+              <a
+                href="https://www.whatsapp.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaWhatsapp />
+              </a>
+            </SocialIcons>
+          </RightContent>
+        </Container>
+        <Ultimo>
+          Copyright © 2024 MAQUITEXT Ecuador.Todos los derechos reservados.
+        </Ultimo>
+      </Center>
     </FooterContainer>
   );
 };
