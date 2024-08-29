71fe9376d399968e9c5ea2035e3d126cca2426f6
unknown <jlc.rodriguez316@gmail.com>
2024-08-29 11:05:04 -0500

    nuevo cambio

--- pages/_app.js
+++ pages/_app.js
@@ -36,7 +36,6 @@ export default function App({ Component, pageProps }) {
           <ContentWrapper>
             <Component {...pageProps} />
           </ContentWrapper>
-          <Footer />
           <FooterU />
         </PageContainer>
       </CartContextProvider>
